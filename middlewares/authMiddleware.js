2b80e290a1ea923713097072706d533e64aef81e
Farhad <freelancerfarhad12@gmail.com>
2024-02-02 20:12:50 +0600

    update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,17 +1,22 @@
 const jwt = require("jsonwebtoken");
 
 module.exports.authMiddleware = async (req, res, next) => {
-  const { accessToken } = req.cookies;
-  if (!accessToken) {
-    return res.status(409).json({ message: "Please Login First" });
-  } else {
-    try {
-      const decodeToken = await jwt.verify(accessToken, process.env.SECRET_KEY);
-      req.role = decodeToken.role;
-      req.id = decodeToken.id;
-      next();
-    } catch (error) {
-      return res.status(400).json({ message: "Please Login" });
+  const { authorization } = req.headers;
+  if (authorization) {
+    const token = authorization.split("Bearer ")[1];
+    if (!token) {
+      return res.status(409).json({ message: "Please Login First" });
+    } else {
+      try {
+        const decodeToken = await jwt.verify(token, process.env.SECRET_KEY);
+        req.role = decodeToken.role;
+        req.id = decodeToken.id;
+        next();
+      } catch (error) {
+        return res.status(400).json({ message: "Please Login" });
+      }
     }
+  } else {
+    return res.status(400).json({ message: "Please Login" });
   }
 };
